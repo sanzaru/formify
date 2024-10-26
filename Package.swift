b13b2b7973a892a8d40d439775e21623889e68df
sanzaru <sanzaru@users.noreply.github.com>
2024-10-26 18:36:30 +0200

    Add support for visionOS

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "Formify",
-    platforms: [.iOS(.v16), .macOS(.v13), .tvOS(.v16), .watchOS(.v9)],
+    platforms: [.iOS(.v16), .macOS(.v13), .tvOS(.v16), .watchOS(.v9), .visionOS(.v1)],
     products: [
         .library(
             name: "Formify",
